ae5d977c0aeabbd0aa0ddc6ac17268c3130cd518
Peggy <peggymailula@gmail.com>
2021-09-02 12:11:06 +0200

    Travis

--- test/greetings.test.js
+++ test/greetings.test.js
@@ -4,8 +4,8 @@ const pg = require("pg");
 const Pool = pg.Pool;
 
 
-//const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/greeting';
-const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/greeting';
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/greeting';
+// const connectionString = process.env.DATABASE_URL || 'postgresql://codex:pg123@localhost:5432/greeting';
 ;
 
 const pool = new Pool({
